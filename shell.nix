73819ed698a87420510b9ea94417538d89bb6c64
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-12 18:45:28 -0400

    shell nix

--- shell.nix
+++ shell.nix
@@ -2,15 +2,16 @@ let
 pkgs = import <nixpkgs> { };
 dotfiles = builtins.fetchGit {
     url = "https://github.com/WGUNDERWOOD/dotfiles";
-    rev = "3c909e8614d7f7bc76c3c10fb22c43ebcaa7ecf8";
+    rev = "c45aa4e8452c36fb1321bbff4172446160ca7c77";
 };
 programs = "${dotfiles}/programs/";
 compress-pdf = pkgs.callPackage "${programs}/compress-pdf/compress-pdf.nix" {};
-latexindent-fast = pkgs.callPackage "${programs}/latexindent-fast/latexindent-fast.nix" {};
 long-lines = pkgs.callPackage "${programs}/long-lines/long-lines.nix" {};
 spell-check = pkgs.callPackage "${programs}/spell-check/spell-check.nix" {};
+tex-build = pkgs.callPackage "${programs}/tex-build/tex-build.nix" {};
 tex-check = pkgs.callPackage "${programs}/tex-check/tex-check.nix" {};
-tex-clean = pkgs.callPackage "${programs}/tex-clean.nix" {};
+tex-clean = pkgs.callPackage "${programs}/tex-clean/tex-clean.nix" {};
+tex-fmt = pkgs.callPackage "${programs}/tex-fmt/tex-fmt.nix" {};
 todo-finder = pkgs.callPackage "${programs}/todo-finder/todo-finder.nix" {};
 in pkgs.mkShell {
     buildInputs = [
@@ -20,11 +21,12 @@ in pkgs.mkShell {
         pkgs.poppler_utils
         pkgs.texlive.combined.scheme-full
         compress-pdf
-        latexindent-fast
         long-lines
         spell-check
+        tex-build
         tex-check
         tex-clean
+        tex-fmt
         todo-finder
     ];
 }
